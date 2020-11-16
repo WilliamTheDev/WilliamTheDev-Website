11e5cfc22b726c9aee10d2b2f2e9541b07ec33e0
Glitch (williamthedev) <none>
2020-11-16 07:04:56 +0000

    🎍👷 Updated with Glitch

--- README.md
+++
@@ -1,1 +0,0 @@
-# WilliamTheDev-Website
---
+++ script.js
@@ -0,0 +1,4 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, this my code, but you can learn here: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
