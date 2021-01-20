5e2b5d45bf449e5cfdb375af9a211b6bb3ea73f3
WilliamTheDev <69966592+WilliamTheDev@users.noreply.github.com>
2021-01-20 21:00:47 +1100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-console.log("Hello, this my code, but you can learn here: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
